7af5a7fc3da090e4287ff632903aff2302b041ea
Sean Snel <SE.Snel@student.han.nl>
2021-02-16 17:35:36 +0100

    implement lift

---
+++ Gnomepunk/Assets/Lift.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class Lift : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed = 1;
+    [SerializeField] private float[] liftHeights;
+ 
+    private Rigidbody _rigidbody;
+    private const float PRECISION = 0.02f;
+    private int _nextHeightIndex = 0;
+
+    public void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            MoveLift();
+        }
+    }
+
+    public void MoveLift()
+    {
+        if (liftHeights.Length < _nextHeightIndex + 1)
+        {
+            //End of level
+        }
+        else
+        {
+            float nextHeight = liftHeights[_nextHeightIndex++];
+            Vector3 newPosition = transform.position;
+            newPosition.y = nextHeight;
+            StartCoroutine(nameof(MoveLiftToLocation), newPosition);
+        }
+    }
+
+    private IEnumerator MoveLiftToLocation(Vector3 targetLocation)
+    {
+        while (transform.position != targetLocation)
+        {
+            transform.position = Vector3.Lerp(transform.position, targetLocation, Time.deltaTime * movementSpeed);
+            if (Vector3.Distance(transform.position, targetLocation) <= PRECISION)
+            {
+                transform.position = targetLocation;
+            }
+            yield return null;
+        }
+    }
+}
