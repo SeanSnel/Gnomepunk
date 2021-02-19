14ca6a68cfb7a0dd199887f6cf90e061bfd30c66
Tim Klaassen <timklaassen@hotmail.nl>
2021-02-19 16:09:00 +0100

    improved rotator

--- Gnomepunk/Assets/Scripts/Rotator.cs
+++ Gnomepunk/Assets/Scripts/Rotator.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 public class Rotator : MonoBehaviour
 {
     public bool start = false;
-    public float speed;
-    public float rotation;
+    public float speed = 0;
+    public float rotationFactorX = 0;
+    public float rotationFactorY = 0;
+    public float rotationFactorZ = 0;
 
     // Update is called once per frame
     void Update()
     {
         if (start)
         {
-            transform.Rotate(new Vector3(0, 0, speed) * Time.deltaTime * 20);
+            transform.Rotate(new Vector3(speed * rotationFactorX, speed * rotationFactorY, speed * rotationFactorZ) * Time.deltaTime * 20);
         }
     }
 
